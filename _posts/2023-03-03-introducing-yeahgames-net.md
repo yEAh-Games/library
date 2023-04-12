4819c3fc1139c071647afd57c926dabe864bfdcb
yEAh-Games <tallinnquinton@gmail.com>
2023-04-11 21:14:51 -0400

    Syncing posts with collections + other fixes
    Collections synced as of 11/04/2023:
    - News
    - Artifacts
    To do:
    - Legal
    - Docs
    
    Other fixes:
    - Cleanup
    - Fixing scripts

---
+++ _posts/2023-03-03-introducing-yeahgames-net.md
@@ -0,0 +1,31 @@
+---
+layout: post
+title:  "Introducing yeahgames.net!"
+author: nnillat
+c: News
+categories: [News/announcements, Official, Network]
+image: /news.yeahgames.net/archive/img/yeahgamesnet.png
+link: https://news.yeahgames.net/news/introducing-yeahgames-net
+tags: [network]
+canonical_url: https://news.yeahgames.net/news/introducing-yeahgames-net
+---
+
+#### Introduction:
+
+In the modern world, online gaming has become one of the most popular forms of entertainment for people of all ages. With the rapid growth of technology, people can now access a variety of games online, anytime and anywhere. The website "yeahgames.net" is one such platform that offers an extensive library of games, quotes, documents, and Yible chapters. Moreover, it has a built-in chat to bypass school restrictions, and offers a plethora of Yeah Very Fungible Tokens (YVFTs) to enhance the user experience.
+
+#### Library:
+
+The website "yeahgames.net" boasts a vast library of games, quotes, documents, and Yible chapters. The game library includes a wide range of genres such as action, adventure, strategy, puzzle, and many more. The website provides an easy-to-use interface that allows users to navigate the game library effortlessly. The quotes library is also extensive, featuring inspiring quotes from famous personalities around the world. The documents library includes a variety of materials such as educational articles, research papers, and informative books. Additionally, the website offers a selection of Yible chapters, which are short stories with a moral lesson that helps to impart valuable life lessons.
+
+#### Built-in chat:
+
+The built-in chat feature on "yeahgames.net" is a valuable tool for users to communicate and connect with others. The chat allows users to send messages, images, and videos, and it's an excellent way to socialize and make new friends. Moreover, the chat is designed to bypass school restrictions, which means that students can access the website and chat with their peers during school hours.
+
+#### yEAh Very-Fungible Tokens:
+
+The website "yeahgames.net" offers a unique feature called Yeah Very Fungible Tokens (YVFTs). These tokens are a form of digital currency that users can earn by playing games or engaging with the website's content. Users can use YVFTs to purchase exclusive game items, access premium features, and participate in contests and giveaways. This innovative feature adds an extra layer of excitement and engagement to the overall user experience.
+
+#### Conclusion:
+
+In summary, the website "yeahgames.net" is an excellent platform for online gaming enthusiasts. With its extensive library of games, quotes, documents, and Yible chapters, users are sure to find something to suit their preferences. The built-in chat feature allows users to socialize and make new friends, while the Yeah Very Fungible Tokens add an extra layer of excitement and engagement. Overall, the website is a well-designed and user-friendly platform that provides a unique and enjoyable online gaming experience.
--- canonical.py
+++ canonical.py
@@ -1,50 +1,36 @@
 import os
 import re
+import glob
 
-def add_canonical_url_to_file(filepath):
-    with open(filepath, 'r') as f:
-        content = f.read()
+directory = 'C:/Users/talli/Documents/GitHub/library/_posts'
 
-    # Find the front matter block using regex
-    match = re.search(r'^---\n(.*\n)*?---\n', content, re.MULTILINE)
-    if not match:
-        # No front matter block found, do nothing
-        return
+front_matter_regex = r'^---\n([\s\S]*?)\n---\n([\s\S]*)'
 
-    front_matter_block = match.group(0)
-    front_matter_vars = {}
+variable_regex = r'^(token|link):\s*(.*)$'
 
-    # Parse front matter block into a dictionary
-    for line in front_matter_block.split('\n'):
-        line = line.strip()
-        if line.startswith('#'):
-            continue
-        if ':' in line:
-            key, value = line.split(':', 1)
-            front_matter_vars[key.strip()] = value.strip()
+for file_path in glob.glob(os.path.join(directory, '*.md')):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        file_contents = file.read()
 
-    # Determine if link or token variable exists and set canonical_url accordingly
-    if 'link' in front_matter_vars:
-        canonical_url = front_matter_vars['link']
-    elif 'token' in front_matter_vars:
-        canonical_url = 'https://login.yeahgames.net/token/' + front_matter_vars['token']
-    else:
-        # No link or token variable found, do nothing
-        return
+    match = re.match(front_matter_regex, file_contents)
+    if match:
+        front_matter = match.group(1)
+        content = match.group(2)
 
-    # Add canonical_url variable to front matter block
-    front_matter_block += f'canonical_url: {canonical_url}\n'
+        token_match = re.search(variable_regex, front_matter, re.MULTILINE)
+        token_value = token_match.group(2) if token_match and token_match.group(1) == 'token' else None
+        link_match = re.search(variable_regex, front_matter, re.MULTILINE)
+        link_value = link_match.group(2) if link_match and link_match.group(1) == 'link' else None
 
-    # Write updated content to file
-    with open(filepath, 'w') as f:
-        f.write(front_matter_block + content[len(match.group(0)):])
+        if token_value:
+            canonical_url_str = 'canonical_url: https://login.yeahgames.net/token/{}\n'.format(token_value)
+            front_matter = front_matter.strip() + '\n' + canonical_url_str
+        elif link_value:
+            canonical_url_str = 'canonical_url: {}\n'.format(link_value)
+            front_matter = front_matter.strip() + '\n' + canonical_url_str
 
-def add_canonical_urls_to_directory(directory):
-    # Loop over all Markdown files in directory
-    for filename in os.listdir(directory):
-        if filename.endswith('.md'):
-            filepath = os.path.join(directory, filename)
-            add_canonical_url_to_file(filepath)
-
-# Example usage: add canonical_url variable to all Markdown files in the current directory
-add_canonical_urls_to_directory('.')
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write('---\n')
+            file.write(front_matter.strip())
+            file.write('\n---\n')
+            file.write(content)
